796e2ba120f1debc7a6ad0a860b624e07233d7f3
root <937791848@qq.com>
2015-07-10 01:06:09 +0800

    commit string part

--- basic/strcat.c
+++ basic/strcat.c
@@ -4,5 +4,11 @@
 
 int main()
 {
+    char ch1[SIZE] = "Linux C";
+    char ch2[SIZE] = " is good~~";
+    strncat(ch1, ch2, strlen(ch2));
+    printf("%s\n", ch1);
+    printf("%s\n", ch2);
 
+    return 0;
 }
