8b68bf5ec173bc1b5ed1ec8f82a5b20890655420
zengqiang <937791848@qq.com>
2015-11-05 15:02:38 +0800

    add network header file

--- network/tcp_client.c
+++ network/tcp_client.c
@@ -1,10 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include "unp.h"
+
 #define SERVER_PORT 10000
 #define SIZE 128
 
--- network/tcp_server.c
+++ network/tcp_server.c
@@ -1,12 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include "unp.h"
+
 #define SERVER_PORT 10000
 #define LENGTH 10
 #define SIZE 128
